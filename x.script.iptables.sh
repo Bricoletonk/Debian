6dbea6bbd66e59f55764dbdcb11274a7ffc95e65
Bricoletonk <78172359+Bricoletonk@users.noreply.github.com>
2025-06-13 14:56:07 +0200

    Update x.script.iptables.sh
    Accès internet pour le firewall

--- x.script.iptables.sh
+++ x.script.iptables.sh
@@ -50,6 +50,18 @@ iptables -A OUTPUT -o $CARTE_EXT -p tcp --sport 22222 -m state --state ESTABLISH
 iptables -A INPUT  -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
+# <-- Le firewall peut faire des requêtes sortantes vers Internet
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+
+# ICMP (ping)
+iptables -A OUTPUT -p icmp -j ACCEPT
+
+# Autoriser les réponses aux connexions initiées par le firewall
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 # ====================
 #  --> NAT : PREROUTING (DNAT) 
 # ====================
